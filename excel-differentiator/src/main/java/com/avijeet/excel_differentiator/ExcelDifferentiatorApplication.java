c56cbe65b4ef19197fbd87529080a4999c9ad332
Avijeet Nayak <avijeetnayak001@gmail.com>
2024-06-18 00:05:16 +0530

    springboot backend setup

---
+++ excel-differentiator/src/main/java/com/avijeet/excel_differentiator/ExcelDifferentiatorApplication.java
@@ -0,0 +1,13 @@
+package com.avijeet.excel_differentiator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelDifferentiatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelDifferentiatorApplication.class, args);
+	}
+
+}
---
+++ excel-differentiator/src/test/java/com/avijeet/excel_differentiator/ExcelDifferentiatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avijeet.excel_differentiator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelDifferentiatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
